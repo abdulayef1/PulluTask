fd127f29aae6ef0633af12db68441b067b0e0727
ibramila <70vq7w9@code.edu.az>
2022-11-30 13:01:32 +0400

    pullu task created adama 5 manat

---
+++ ConsoleApp4/MyList.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp4;
+
+public class MyList<T>
+{
+    private static int count = 0;
+
+    public int Capacity { get; set; }
+    public int Count { get => count; }
+    T[] items;
+    bool isLast=true;
+    public MyList()
+    {
+        Capacity = 0;
+    }
+    public MyList(int capacity)
+    {
+        Capacity = capacity;
+    }
+
+    public void Add(T item)
+    {
+        if (Capacity == 0)
+        {
+            Capacity = 2;
+            Array.Resize(ref items, Capacity);
+            items[0] = item;
+            count++;
+            return;
+        }
+        if (Capacity == Count)
+        {
+            Capacity = Capacity * 2;
+            Array.Resize(ref items, Capacity);
+            items[Capacity / 2]=item;
+            count++;
+            return;
+        }
+        //[1,1,1,1]
+        items[Count] = item;
+        count++;
+    }
+}
+
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿//List<int> numbers = new List<int>();
+
+//numbers.Add(0);
+//numbers.Add(1);
+//numbers.Add(2);
+//Console.WriteLine(numbers.Exists(item => item == 8));
+
+using ConsoleApp4;
+
+List<int> ints = new List<int>()
+{
+    1,2,3
+};
+
+MyList<int> myList = new MyList<int>();
+
+myList.Add(1);
+myList.Add(1);
+myList.Add(1);
+myList.Add(1);
+myList.Add(1);
+
+Console.WriteLine(myList.Capacity);
